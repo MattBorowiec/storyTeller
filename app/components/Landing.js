5b4a9dd09702b07c200df17cc92a6daca22672ff
Matt Borowiec <mborowiec@gmail.com>
2017-01-07 14:45:24 -0800

    Use shiny new graphics for landing.

--- app/components/Landing.js
+++ app/components/Landing.js
@@ -1,77 +1,43 @@
 import React, { Component } from 'react';
 import { connect } from 'react-redux';
-import { StyleSheet, View, Image, Text, TouchableOpacity } from 'react-native';
+import { StyleSheet, View, Image, Text, TouchableHighlight } from 'react-native';
 import Dimensions from 'Dimensions';
 import { Actions } from 'react-native-router-flux';
 
 
-
 class Landing extends Component {
-  constructor (props) {
-    super(props)
-  }
+    constructor(props) {
+        super(props)
+    }
+
+    _onPressButton() {
+        Actions.StoryList();
+    }
 
 
-  componentDidUpdate() {
-    // console.log(this.props);
-  }
-
-  _onPressButton() {
-    Actions.StoryList();
-  }
-
-
-  render() {
-    const play = require('../../img/play-icon.png');
-    return (
-        <View>
-          <Image source={require('../../img/background.jpeg')} style={styles.background} />
-          <View style={styles.textContainer}>
-            <Text style={[styles.text, {fontSize: 40}]}>Listening Matters</Text>
-            <Text style={[styles.text, {fontSize: 35, marginBottom: 50}]}>Everyone deserves to be heard.
-              Stories Connect us.</Text>
-            <TouchableOpacity onPress={this._onPressButton.bind(this)}>
-              <Text style={[styles.text, styles.listenButton]}>Tap to listen.</Text>
-            </TouchableOpacity>
-          </View>
-        </View>
-
-    );
-  }
+    render() {
+        const play = require('../../img/play-icon.png');
+        return (
+            <View>
+                <TouchableHighlight onPress={this._onPressButton.bind(this)}>
+                    <Image source={require('../../img/background.png')} style={styles.background}/>
+                </TouchableHighlight>
+            </View>
+        );
+    }
 }
 
 const styles = StyleSheet.create({
-  text: {
-    color: '#d3d9df',
-    textShadowColor: 'black',
-    textShadowOffset: {width: 2, height: 2},
-    textShadowRadius: 2
-  },
-  listenButton: {
-    fontSize: 40,
-    color: '#2f9c0a'
-  },
-  textContainer: {
-    zIndex: 2,
-    flex: 1,
-    alignItems: 'center',
-    justifyContent: 'center',
-    width: Dimensions.get('window').width,
-    height: Dimensions.get('window').height,
-  },
-  background: {
-    alignSelf: 'center',
-    zIndex: -1,
-    position: 'absolute',
-    height: Dimensions.get('window').height,
-    width: Dimensions.get('window').width
-  }
+    background: {
+        height: Dimensions.get('window').height,
+        width: Dimensions.get('window').width
+    }
 });
 
 const mapStateToProps = (state) => {
-  return {
-    state: state,
-  }
+    return {
+        state: state
+    }
 };
 
 export default connect(mapStateToProps)(Landing);
