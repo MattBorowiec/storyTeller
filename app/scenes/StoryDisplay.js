d2d356616d26cb151ac1fda8eb27f8ce32290adc
Matt Borowiec <mborowiec@gmail.com>
2017-03-29 14:15:14 -0700

    Remove related styles

--- app/scenes/StoryDisplay.js
+++ app/scenes/StoryDisplay.js
@@ -69,28 +69,7 @@ const styles = {
     },
     scrollContainer: {
         paddingLeft: 40
-    },
-    closePlayer: {
-        color: "gray",
-        fontSize: 50,
-        fontFamily: "curious",
-        marginBottom: 6,
-        zIndex: 1
-    },
-    closeContainer: {
-        flex: 1,
-        justifyContent: "center",
-        alignItems: "center",
-        position: "absolute",
-        width: 50,
-        height: 50,
-        borderWidth: 4,
-        borderRadius: 25,
-        borderColor: "gray",
-        top: 10,
-        right: 15,
-        zIndex: 2
-    },
+    }
 };
 
 
